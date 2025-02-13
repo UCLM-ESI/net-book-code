9b8a38d4088cf818549bfcf170cfab236029a547
David Villa Alises <david.villa@gmail.com>
2025-02-13 20:38:21 +0100

    tmux runner

--- media-streaming/run.sh
+++ media-streaming/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-tmux new-session -d -s session
-tmux split-window -h
-tmux send-keys -t session:0.0 './server.py 2000 | mplayer -quiet -' C-m
-tmux send-keys -t session:0.1 'sleep 2; ./client.py localhost 2000 < audio.mp3' C-m
-tmux attach-session -t session
+tmux -f .tmux.conf new-session -d -s session
+tmux -f .tmux.conf split-window -h
+tmux -f .tmux.conf send-keys -t session:0.0 './server.py 2000 | mplayer -msglevel all=-1 -' C-m
+tmux -f .tmux.conf send-keys -t session:0.1 'sleep 2; ./client.py localhost 2000 < song.mp3' C-m
+tmux -f .tmux.conf attach-session -t session
