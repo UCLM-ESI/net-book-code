3e5b8c03177556b85c51d28c4c1aa1045f7055d0
David Villa Alises <david.villa@gmail.com>
2025-02-13 19:59:35 +0100

    tmux runner

--- flow-control/run.sh
+++ flow-control/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-tmux new-session -d -s session
-tmux split-window -h
-tmux send-keys -t session:0.0 './server.py 2000' C-m
-tmux send-keys -t session:0.1 'sleep 1; ./client.py localhost 2000' C-m
-tmux attach-session -t session
+tmux -f .tmux.conf new-session -d -s session
+tmux -f .tmux.conf split-window -h
+tmux -f .tmux.conf send-keys -t session:0.0 "./server.py 2000 500" C-m
+tmux -f .tmux.conf send-keys -t session:0.1 "sleep 1; ./client.py 127.0.0.1 2000" C-m
+tmux -f .tmux.conf attach-session -t session
