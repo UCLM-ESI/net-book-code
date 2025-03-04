de65cfbe7eab702a7f90c24e5ca8b210a91539d9
David Villa Alises <david.villa@gmail.com>
2025-03-04 13:51:30 +0100

    static routing OK

---
+++ routing-docker-lab/setup-static.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker compose exec r01 ip route add default via 10.0.1.3
+docker compose exec r12 ip route add 10.0.0.0/24 via 10.0.1.2
+docker compose exec r12 ip route add 10.0.3.0/24 via 10.0.2.3
+docker compose exec r23 ip route add default via 10.0.2.2
+docker compose exec server ip route add default via 10.0.3.2
