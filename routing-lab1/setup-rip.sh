b89f448182c150742918640f14910fbdebd768b3
David Villa Alises <david.villa@gmail.com>
2025-03-09 14:19:38 +0100

    rip config

--- routing-lab1/setup-rip.sh
+++ routing-lab1/setup-rip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
 
 for router in r01 r12 r23; do
-    docker compose exec $router /usr/lib/frr/ripd -f /etc/frr/ripd.conf -d
+    docker exec $router /usr/lib/frr/ripd -f /etc/frr/ripd.conf -d
 done
