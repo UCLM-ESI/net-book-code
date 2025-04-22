8d747b2314138b0b05f60d5b46886e2c23ccb413
David Villa Alises <david.villa@gmail.com>
2025-04-22 14:16:34 +0200

    typo

--- routing-lab1/setup-hosts.sh
+++ routing-lab1/setup-hosts.sh
@@ -3,5 +3,5 @@
 # add route for 10.0/16 for "host"
 ip route add 10.0.0.0/16 via 10.0.0.3
 
-# add "server" default route for
+# add "server" default route
 docker exec server ip route add default via 10.0.3.2
