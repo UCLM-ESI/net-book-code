a10de9a262bf8a4732e0ff0227d722cdbab9be4d
David Villa Alises <david.villa@gmail.com>
2025-04-30 14:49:56 +0200

    working on frr mcast

---
+++ lab-routing-mcast/check.sh
@@ -0,0 +1,2 @@
+docker exec -ti r1 vtysh -c "show running-config"
+docker exec -ti r1 ps fax
--- lab-routing-mcast/setup-pim.sh
+++ lab-routing-mcast/setup-pim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
 
 for router in r1 r2 r3; do
-    docker exec $router /usr/lib/frr/pimd -f /etc/frr/pimd.conf -d
+    docker exec $router /usr/lib/frr/frrinit.sh start
 done
